f7824043de913390e4f6bf6f419d6b3c035b6fb5
Chirag-Javis <chirag.yaduwanshi@javis.ai>
2023-09-14 12:14:20 +0530

    commit 34

--- app/main.py
+++ app/main.py
@@ -1,15 +1,35 @@
-# This is a sample Python script.
-# Granted Lambda access in 32' commit
-
-
 def lambda_handler(event, context):
-    # Use a breakpoint in the code line below to debug your script.
-    a = event['a']
-    b = event['b']
-    response = {
-        "statusCode": 200,
-        "body": f"addition : {a} + {b} = {a+b}"
-    }
-    print('a + b = ', a+b)
-    return response
+    try:
+        # Extract 'a' and 'b' from the event dictionary
+        a = event['a']
+        b = event['b']
 
+        # Perform the addition
+        result = a + b
+
+        # Create a successful response dictionary
+        response = {
+            "statusCode": 200,
+            "body": f"addition: {a} + {b} = {result}"
+        }
+
+        # Print the result for debugging (visible in CloudWatch Logs)
+        print('a + b =', result)
+
+        return response
+    except KeyError as e:
+        # Handle missing 'a' or 'b' in the event
+        error_message = f"Missing key in event: {e}"
+        print(error_message)
+        return {
+            "statusCode": 400,
+            "body": error_message
+        }
+    except Exception as e:
+        # Handle other exceptions
+        error_message = f"An error occurred: {e}"
+        print(error_message)
+        return {
+            "statusCode": 500,
+            "body": error_message
+        }
