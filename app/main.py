e224d0f7e6cb887a62ebd3da164d91777b981050
Chirag-Javis <chirag.yaduwanshi@javis.ai>
2023-10-11 12:02:19 +0530

    Merge test 9

--- app/main.py
+++ app/main.py
@@ -1,23 +1,21 @@
 
-#Trail Number 07
 def lambda_handler(event, context):
     try:
         a = event['a']
         b = event['b']
 
-        result = a + b
+        result = a * b
 
         response = {
             "statusCode": 200,
-            "body": f"addition: {a} + {b} = {result}"
+            "body": f"multiplication: {a} * {b} = {result}"
         }
 
-        # Print the result for debugging (visible in CloudWatch Logs)
-        print('a + b =', result)
+        print('a * b =', result)
 
         return response
     except KeyError as e:
-        # Handle missing 'a' or 'b' in the event
+
         error_message = f"Missing key in event: {e}"
         print(error_message)
         return {
@@ -25,7 +23,7 @@ def lambda_handler(event, context):
             "body": error_message
         }
     except Exception as e:
-        # Handle other exceptions
+
         error_message = f"An error occurred: {e}"
         print(error_message)
         return {
